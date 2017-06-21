7d1cd4a76a816a0589d3ca0dd47d9dd2a7c2b765
abhisheksingla <apple@Abhisheks-Mac-Mini.local>
2017-06-21 18:24:04 +0530

    Sheduler in progress

--- Abkao/Interfaces/SchedulerI.swift
+++ Abkao/Interfaces/SchedulerI.swift
@@ -10,4 +10,41 @@ import UIKit
 
 class SchedulerI: NSObject {
 
+    public var productVedUrl: String?
+    public var scheduleID : Int?
+    public var arrDays : [String]?
+    public var startTime: String?
+    public var endTime: String?
+    
+    
+    
+    override init(){
+        self.productVedUrl = ""
+        self.scheduleID = 0
+        self.arrDays = [String]()
+        self.startTime = ""
+        self.endTime = ""
+    }
+    
+    
+    func resetData()
+    {
+        self.productVedUrl = ""
+        self.scheduleID = 0
+        self.arrDays?.removeAll()
+        self.startTime = ""
+        self.endTime = ""
+    }
+    
+    
+    public func setSchedules(scheduleObj : [String : AnyObject])
+    {
+        self.productVedUrl =  (scheduleObj["video_link"] as? String ?? "")
+        self.scheduleID = Int((scheduleObj["scheduler_id"] as? String)!)
+        self.startTime = (scheduleObj["start_time"] as? String)!
+        self.endTime = (scheduleObj["end_time"] as? String)!
+        self.arrDays = scheduleObj["days"] as? NSArray as? [String]        
+        
+    }
+    
 }
